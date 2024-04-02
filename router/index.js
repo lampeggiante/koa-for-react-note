e6d265b4a9992a60b23e50bdafb054e17de85324
LeonYu <1099052496@qq.com>
2024-04-03 00:46:30 +0800

    feat: 加入路由、校验、错误处理等第三方库

--- router/index.js
+++ router/index.js
@@ -6,6 +6,10 @@ router.get('/test', async (ctx) => {
   ctx.body = 'test success!'
 })
 
+router.get('/', async (ctx) => {
+  ctx.body = 'hello world'
+})
+
 router.post('/post_test', async (ctx) => {
   ctx.body = ctx.request.body
 })
