d5563830ee24eea4e18b2df5552bb76982dfa1d9
LeonYu <1099052496@qq.com>
2024-04-03 00:46:30 +0800

    feat: 加入路由、校验、错误处理等第三方库

--- app.js
+++ app.js
@@ -1,30 +1,68 @@
-import Koa from 'koa'
-import path from 'path'
-import bodyParser from 'koa-bodyparser'
+import Koa from "koa"
+import path from "path"
+import KoaLogger from "koa-logger" // koa-log4 似乎更好
+import koaStatic from "koa-static"
+import { koaBody } from "koa-body"
+import KoaBouncer from "koa-bouncer"
+import onError from "koa-onerror"
+import KoaSession from "koa-session"
 
 import config from "./config/index.js"
-import router from './router/index.js'
+import router from "./router/index.js"
+import { accessLogger, serverLogger, logger } from "./utils/logger.js"
+import { CustomException } from "./utils/exception.js"
+import { SESSION_CONFIG } from "./config/index.js"
 
 const app = new Koa()
 
-app.use(bodyParser(/* {
-  enableTypes: ['json', 'text'], // 仅允许 json 和 text
-  strict: true,
-  formidableOptions: {          // 如果需要处理文件上传，这里可以配置 formidable 的选项
-    uploadDir: path.join(__dirname, '/upload'), // 指定上传目录
-    keepExtensions: true         // 保持文件扩展名
-  },
-  onError: function (err, ctx) {
-    ctx.throw(400, err);
-  },
-  patch: true                    // 尝试修复某些错误 
-} */))
+app.keys = ["koa_for_react_blog"]
 
-// 注册路由
-app.use(router.routes())
+app
+  .use(KoaSession(SESSION_CONFIG, app))
+  .use(accessLogger())
+  .use(KoaLogger())
+  .use(
+    koaBody({
+      enableTypes: ["json", "form", "text", "xml"],
+      onError: (err, ctx) => {
+        ctx.throw(422, "body parse error", err)
+      },
+    }),
+  )
+  .use(KoaBouncer.middleware()) // 加入 bouncer 可以支持校验表单
+  .use(koaStatic(path.join(path.resolve(), config.staticPath)))
+  .use(router.routes()) // 注册路由
 
-app.listen(config.port)
-console.log(`
-  后台已启动，url为: ${config.ip}:${config.port}
-  或者也可以访问:    http://localhost:${config.port}
-`)
+onError(app, {
+  JSON: (err, ctx) => {
+    if (err instanceof KoaBouncer.ValidationError) {
+      ctx.status = 400
+    } else {
+      ctx.status = err.status || 500
+    }
+
+    ctx.body = {
+      code: ctx.status,
+      message: err.message || "server error",
+      data: null,
+    }
+  },
+})
+
+app.on("error", (err, ctx) => {
+  // 判断校验类型错误
+  if (err instanceof KoaBouncer.ValidationError) return logger.warn(err)
+  // 如果是自定义错误
+  if (err instanceof CustomException) return logger.warn(err)
+  // 错误日志持久化
+  // logger.error(err);
+  serverLogger.error(`Error on ${ctx.method} ${ctx.url}: ${err}`)
+})
+
+const server = app.listen(config.port, config.ip, () => {
+  const address = server.address()
+  console.log(`
+    后台已启动，url为: http://${address.address}:${address.port}
+    或者也可以访问:    http://localhost:${address.port}
+  `)
+})
--- config/index.js
+++ config/index.js
@@ -1,5 +1,12 @@
 export default {
-  ip: 'http://127.0.0.1',
-  port: '3000',
-  staticPath: '/public',
+  ip: "127.0.0.1",
+  port: "3000",
+  staticPath: "/public",
+}
+
+export const SESSION_CONFIG = {
+  key: "koa-for-react-note", // 设置cookie的key的名字
+  maxAge: 1000 * 60 * 60 * 1, // 设置有效期为一个小时
+  httpOnly: true, // 仅服务端修改
+  signed: true, // 签名cookie
 }
